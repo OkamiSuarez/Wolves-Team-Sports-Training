d584945963f2293232aca8ebd49aa640c1da51c2
OkamiSuarez <okami97.backdev@hotmail.com>
2022-08-18 15:29:36 -0500

    Se añade bootstrap con un carrusel y la store para entrega

--- scripts/store.js
+++ scripts/store.js
@@ -9,13 +9,13 @@ products.forEach((product) => {
         <div class="card-header">
             <h2 class="card-title">${product.name}</h5>
         </div>
-        <div class="card-body">
-            <img src="${product.img}" class="card-img-top" alt="...">
-            <h3 class="card-text">Price: ${product.price}</h3>
-            <h5 class="card-text">Brand: ${product.brand}</h5>
-            <p class="card-text">Description: ${product.description}</p>
-            <a href="${product.video}" target="blank" class="text-info stretched-link" id="esParaTi">Is this product for me?</a>
-        </div>
+          <div class="card-body">
+              <img src="${product.img}" class="card-img-top" alt="...">
+              <h3 class="card-text">Price: ${product.price}</h3>
+              <h5 class="card-text">Brand: ${product.brand}</h5>
+              <p class="card-text">Description: ${product.description}</p>
+              <a href="${product.video}" target="blank" class="text-info stretched-link" id="esParaTi">Is this product for me?</a>
+          </div>
         <div class="card-footer">
             <button type="button" class="btn btn-success" onclick="buyProduct(${product.id})" id="botonDeCompra${product.id}">Buy now</button>
         </div>
